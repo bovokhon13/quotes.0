5dd36ebad3482f627c90efe7733dd86f84f3e4c9
bovokhon13 <bovokhonmukarramov3@gmail.com>
2025-06-08 21:12:07 +0300

    Create settings.py

---
+++ config/settings.py
@@ -0,0 +1,8 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+ADMIN_IDS = [int(id) for id in os.getenv("ADMIN_IDS", "").split(",") if id]
+API_URL = "https://api.quotable.io/random"  # Quotable API
